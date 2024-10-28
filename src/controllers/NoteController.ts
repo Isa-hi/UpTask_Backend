c32ee1529e241adb04ae905736481e04ce2886b4
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-10-28 15:56:39 -0600

    Refactor NoteController to include deleteNote method

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import Note, { INote } from "../models/Note";
+import { Types } from "mongoose";
+
+type NoteParams = {
+    noteId: Types.ObjectId;
+}
 
 export class NoteController {
     static createNote = async (req: Request<{},{}, INote>, res: Response) => {
@@ -24,6 +29,30 @@ export class NoteController {
         try {
             const notes = await Note.find({ task: req.task.id });
             res.json(notes);
+        } catch (error) {
+            res.status(500).json({ message: "Server error" });
+        }
+    }
+
+    static deleteNote = async (req: Request<NoteParams>, res: Response) => {
+        const { noteId } = req.params;
+        const note = await Note.findById(noteId);
+
+        if(!note) {
+            const error = new Error("Note not found");
+            res.status(404).json({ message: error.message });
+            return
+        }
+
+        if(note.createdBy.toString() !== req.user.id) {
+            res.status(403).json({ message: "You are not authorized to delete this note" });
+            return
+        }
+
+        try {
+            req.task.notes = req.task.notes.filter((n) => n.toString() !== note.id);
+            await Promise.allSettled([note.deleteOne(), req.task.save()]);
+            res.send("Note deleted successfully");
         } catch (error) {
             res.status(500).json({ message: "Server error" });
         }
