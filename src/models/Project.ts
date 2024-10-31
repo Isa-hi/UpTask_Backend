9fe5502ca0a95a253aa277bd93b4cc95b1eb6e9d
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-10-31 17:38:51 -0600

    Implement cascading delete for Project and Task models to remove associated Notes

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
-import { ITask } from "./Task";
+import Task, { ITask } from "./Task";
 import { IUser } from "./User";
+import Note from "./Note";
 
 //TypeScript type for Project
 export interface IProject extends Document {
@@ -42,6 +43,17 @@ const ProjectSchema = new Schema({
         ref: "User"
     }]
 },{timestamps: true})
+
+ProjectSchema.pre('deleteOne', { document: true }, async function() {
+    const projectId = this._id;
+    if(!projectId) return;
+
+    const tasks = await Task.find({ project: projectId });
+    for(const task of tasks){
+        await Note.deleteMany({ task: task._id });
+    }
+    await Project.deleteMany({ project: projectId });
+  });
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema);
 export default Project;
